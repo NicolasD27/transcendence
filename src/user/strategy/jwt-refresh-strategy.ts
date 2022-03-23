5c0a861810daadabcb8823446d771e640150144a
Nicolas Descreux <nicolasdescreux@hotmail.fr>
2022-03-23 12:58:09 +0100

    fix users repo

--- src/user/strategy/jwt-refresh-strategy.ts
+++ src/user/strategy/jwt-refresh-strategy.ts
@@ -3,18 +3,20 @@ import { PassportStrategy } from "@nestjs/passport";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from "../interface/jwt-payload.interface";
-import { UserRepository } from "../repository/user.repository";
 import { AuthService } from "../service/auth.service";
 
 import * as config from 'config'
+import { Repository } from "typeorm";
+import { User } from "../entity/user.entity";
+
 
 const dbConfig = config.get('jwt')
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh-token') {
     constructor(
-        @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
+        @InjectRepository(User)
+        private  usersRepository: Repository<User>,
         private authService: AuthService
     ){
         super({
