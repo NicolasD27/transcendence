9bbc601e0c533b6eacfb5bc4c6cb597265f8729f
ChevalierSoft <ibanarashi@outlook.fr>
2022-03-25 10:40:05 +0100

    using a guard to check if the client is logged before giving him previous messages

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -3,13 +3,13 @@ import { ChatService } from "./service/chat/chat.service";
 import { FtOauthGuard } from "src/guards/ft-oauth.guard";
 import { AuthGuard } from "@nestjs/passport";
 import { CreateMsgDto } from "./dto/create-msg.dto";
-
+import { AuthenticatedGuard } from "src/guards/authenticated.guard";
 
 @Controller('channels/:id')
 export class ChatController {
 	constructor (private readonly chatService: ChatService) {}
 
-	// @UseGuards(FtOauthGuard)	// ? need a fix : loops on login
+	@UseGuards(AuthenticatedGuard)	// ? need a fix : loops on login
 	@Get()
 	async findAll() {
 		return this.chatService.getAllMessages();
