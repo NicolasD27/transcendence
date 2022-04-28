a7f17638e1ff49f9a4fc37302123fdaae0d2a33f
ChevalierSoft <ibanarashi@outlook.fr>
2022-04-28 13:23:18 +0200

    patched the socket.leave specifying the room the client have to leave

--- Backend/src/message/gateway/message.gateway.ts
+++ Backend/src/message/gateway/message.gateway.ts
@@ -1,4 +1,4 @@
-import { UseGuards, Logger, Request, Req } from '@nestjs/common';
+import { Logger, Request } from '@nestjs/common';
 import {
 	OnGatewayConnection,
 	OnGatewayDisconnect,
@@ -10,7 +10,6 @@ import {
 import { Socket, Server } from 'socket.io';
 import { CreateMsgDto } from '../dto/create-msg.dto';
 import { ChatService } from '../service/message.service';
-import { WsGuard } from '../../guards/websocket.guard';
 import { ChannelService } from 'src/channel/service/channel.service';
 import { getUsernameFromSocket } from 'src/user/get-user-ws.function';
 import { activeUsers, CustomSocket } from 'src/auth-socket.adapter';
@@ -38,7 +37,6 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 		const username = getUsernameFromSocket(socket);
 		await this.channelService.checkUserJoinedChannelWS(username, data.activeChannelId)
 		.then(()=>{
-			console.log("then");
 			this.chatService.saveMsg(data.content, data.activeChannelId, username)
 			.then((message)=>{
 				const msgDto: CreateMsgDto = {
@@ -51,7 +49,7 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 			.catch(()=>{ return ; });                                           
 		})
 		.catch(()=>{
-			console.log("catch");
+			console.log("checkUserJoinedChannelWS failed");
 			return ;
 		});
 	}
@@ -79,13 +77,13 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 	async pepoLeave(socket: CustomSocket, data: { channelId: string }) {
 		// activeUsers.remove(socket.user.id);
 
-		const findMyUser = await this.server
+		const myClientSocket = await this.server
 			.in(activeUsers.getSocketId(socket.user.id).socketId)
 			.fetchSockets();
-		if (findMyUser.length)
+		if (myClientSocket.length)
 		{
 			console.log(`${socket.user.username} left ${data.channelId}`);
-			findMyUser[0].leave(String(data.channelId));
+			myClientSocket[0].leave("channel#" + data.channelId);
 		}
 	}
 
@@ -94,11 +92,11 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 	}
 	
 	// @UseGuards(WsGuard)
-	async handleConnection(socket: CustomSocket, @Request() req, ...args: any[]) {
-		this.logger.log(`socket connected: ${socket.id}`);
+	async handleConnection(client: CustomSocket, @Request() req, ...args: any[]) {
+		this.logger.log(`${client.user.username} connected via ${client.id}`);
 	}
 
-	handleDisconnect(client: Socket) {
-		this.logger.log(`Client disconnected: ${client.id}`);
+	handleDisconnect(client: CustomSocket) {
+		this.logger.log(`${client.user.username} disconected`);
 	}
 }
