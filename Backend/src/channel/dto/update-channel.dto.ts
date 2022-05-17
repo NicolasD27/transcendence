e01543d643c7494d72d29748a4b8c79686138553
rzafari42 <redwane.zafari@hotmail.com>
2022-05-17 12:09:08 +0200

    file deletion

--- Backend/src/channel/dto/update-channel.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateMsgDto } from "../../chat/dto/create-msg.dto";
-
-export class UpdateMsgDto extends PartialType(CreateMsgDto) {}
--- Backend/src/chat/decorator/get-username.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-// import { User } from "../entity/user.entity";
-
-export const GetUsername = createParamDecorator((data, ctx: ExecutionContext): any => {
-    const user = ctx.switchToWs().getData().author;
-    return user;
-})
