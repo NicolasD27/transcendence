313b870266a368b7b76491a642709cacae3d5a4b
rzafari42 <redwane.zafari@hotmail.com>
2022-05-27 11:24:48 +0200

    channel might work as expected

--- Backend/src/channel/dto/pagination-query.dto.ts
+++ Backend/src/channel/dto/pagination-query.dto.ts
@@ -1,14 +1,17 @@
 import { Type } from "class-transformer";
 import { IsNumber, IsOptional, IsPositive, Min } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationQueryDto
 {
+	@ApiProperty()
 	@IsOptional()
 	@Type(() => Number)
 	@IsNumber()
 	@Min(0)
 	limit: number;
 
+	@ApiProperty()
 	@IsOptional()
 	@Type(() => Number)
 	@IsNumber()
