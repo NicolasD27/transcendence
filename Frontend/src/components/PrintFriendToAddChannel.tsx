b24da2973c7ec3d92bf0294f4e138ed2c4d92a1a
MrSbarre <34176679+MrSbarre@users.noreply.github.com>
2022-06-27 15:19:41 +0200

    Update PrintFriendToAddChannel.tsx

--- Frontend/src/components/PrintFriendToAddChannel.tsx
+++ Frontend/src/components/PrintFriendToAddChannel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Dispatch, SetStateAction, useEffect, Fragment, ReactNode} from 'react'
+import React, { useState, Dispatch, SetStateAction} from 'react'
 import { useNavigate } from 'react-router-dom'
 import statusIconGreen from "../asset/statusIconGreen.svg"
 import statusIconRed from "../asset/statusIconRed.svg"
@@ -13,6 +13,7 @@ interface PropsPrintFriendToAddChannel {
 
 const PrintFriendToAddChannel : React.FC<PropsPrintFriendToAddChannel> = (props) => {
 
+	const [ profileAvatar, setProfileAvatar ] = useState("")
 	const defaultAvatar = 'https://steamuserimages-a.akamaihd.net/ugc/907918060494216024/0BA39603DCF9F81CE0EC0384D7A35764852AD486/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false';
 	const navigate = useNavigate()
 
@@ -31,52 +32,43 @@ const PrintFriendToAddChannel : React.FC<PropsPrintFriendToAddChannel> = (props)
 		}
 	}
 
-
-	const renderOneFriend = (user) => {
-		let statusIcon = (user.status === 1 ? statusIconGreen : statusIconRed)
-		const profileAvatar = `http://${process.env.REACT_APP_HOST || "localhost"}:8000/api/database-files/${user.avatarId}`
-		return (
-			<div className='user' key={user.id}>
-				<div id='userAvatarIcon'>
-					{
-						user.avatarId == null &&
-						<img src={defaultAvatar} className="userAvatar" alt="defaultAvatar" onClick={() => onProfil(user.id.toString())}/>
-					}
-					{
-						user.avatarId != null &&
-						<img src={profileAvatar} className="userAvatar" alt="profileAvatar" onClick={() => onProfil(user.id.toString())}/>
-					}
-					<img src={statusIcon} className="userStatusIcon" alt="StatusIcon"/>
-				</div>
-				<div id="username">{user.username}</div>
-				<div className='checkbox_Channel'>
-					<input type='checkbox' name="addFriendToChannelButton" id={user.id.toString()} onChange={() => checkSelectionStatus(user)}/>
-					<label htmlFor={user.id.toString()}></label>
-				</div>
-			</div>
-		)
-	}
-
-	const renderFriends = () => {
-		return props.friends.filter(friend => {
-			if (friend.id !== props.idMe)
-				return friend;
-			return false
-		})
-		.map((user:FriendsFormat) => {
-			return renderOneFriend(user)
-		})
-	}
-
-
-
-
-
 	return (
-		<Fragment>
-			{renderFriends()}
-		</Fragment>
+		<>
+			{props.friends
+				.filter(friend => {
+					if (friend.id !== props.idMe)
+						return friend;
+					return false
+				})
+				.map((user:FriendsFormat) => {
+					let statusIcon = (user.status === 1 ? statusIconGreen : statusIconRed)
 
+					if (user.avatarId != null)
+						setProfileAvatar(`http://localhost:8000/api/database-files/${user.avatarId}`)
+
+					return (
+						<div className='user' key={user.id}>
+							<div id='userAvatarIcon'>
+								{
+									user.avatarId == null && 
+									<img src={defaultAvatar} className="userAvatar" alt="defaultAvatar" onClick={() => onProfil(user.id.toString())}/>
+								}
+								{
+									user.avatarId != null &&
+									<img src={profileAvatar} className="userAvatar" alt="profileAvatar" onClick={() => onProfil(user.id.toString())}/> 
+								}
+								<img src={statusIcon} className="userStatusIcon" alt="StatusIcon"/>
+							</div>
+							<div id="username">{user.username}</div>
+							<div className='checkbox_Channel'>
+								<input type='checkbox' name="addFriendToChannelButton" id={user.id.toString()} onChange={() => checkSelectionStatus(user)}/>
+								<label htmlFor={user.id.toString()}></label>
+							</div>
+						</div>
+					)
+				})
+			}
+		</>
 	)
 }
 
